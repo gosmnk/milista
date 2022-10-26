d3883e58685d44c185d2f823ecc3eecd2593ea35
gus-codes <gusmnk.dev@gmail.com>
2022-10-26 15:08:07 -0300

    frts

---
+++ milista.js
@@ -0,0 +1,21 @@
+const miLista = {
+    nombre:'Gustavo',
+    edad: 51,
+    desarrollador: true,
+     
+
+
+}
+
+const nacimiento = new Date('may 13 1971 ');
+
+let libroFavorito = {
+    titulo: 'habitos atomicos',
+    autor: 'james Clear',
+    fecha: 2020,
+    url: 'amazon.com'
+}
+
+console.log(nacimiento);
+
+console.log(libroFavorito)
